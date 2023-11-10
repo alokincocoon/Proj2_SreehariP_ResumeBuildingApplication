3aec4e037f741b7f7e13f11164a71fbf35ae573f
sreeharipalokin <sreeharip@alokin.in>
2023-11-10 10:47:13 +0530

    get function added to recieve dict data

--- backend/app.py
+++ backend/app.py
@@ -77,6 +77,8 @@ async def show_data_by_id(field_id: int) -> json:
     all_data["social_media"] = final_data(media_record)
     
     education_record = session.query(Education).filter_by(applicant_details_id=field_id)
+    # print(type(education_record))
+    # print(final_data(education_record))
     # all_data["education"] = final_data(education_record)
 
     work_record = session.query(WorkExperience).filter_by(applicant_details_id=field_id)
@@ -99,55 +101,54 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
     """This function will save the applicant's details 
     to the Database.
     'applicant_details' is a variable which saves data
-    to ApplicantDetails model
+    to ApplicantDetails model.
     """
     applicant_details  = ApplicantDetails(
-        id=data["applicant_details"]["id"], 
-        full_name=data["applicant_details"]["full_name"], 
-        email_id=data["applicant_details"]["email_id"], 
-        phone_number=data["applicant_details"]["phone_number"], 
-        image_url=data["applicant_details"]["image_url"], 
-        summary=data["applicant_details"]["summary"]
+        id=data["applicant_details"].get("id"), 
+        full_name=data["applicant_details"].get("full_name"), 
+        email_id=data["applicant_details"].get("email_id"), 
+        phone_number=data["applicant_details"].get("phone_number"), 
+        image_url=data["applicant_details"].get("image_url"), 
+        summary=data["applicant_details"].get("summary")
     )
     if applicant_details:
         session.add(applicant_details)
 
     #  address_details variable saves data to AddressDetails model
     address_details = AddressDetails(
-        applicant_details_id=data["applicant_details"]["id"],
-        address_line=data["address_details"]["address_line"],
-        street_name=data["address_details"]["street_name"],
-        city=data["address_details"]["city"],
-        country=data["address_details"]["country"],
-        zip_code=data["address_details"]["zip_code"]
+        applicant_details_id=data["applicant_details"].get("id"),
+        address_line=data["address_details"].get("address_line"),
+        street_name=data["address_details"].get("street_name"),
+        city=data["address_details"].get("city"),
+        country=data["address_details"].get("country"),
+        zip_code=data["address_details"].get("zip_code")
     )
     if address_details:
-        print(address_details)
         session.add(address_details)
     
     if type(data["education"]) is list:
         levels_of_education = len(data["education"])
         for entry in range(levels_of_education):
             education = Education(
-                applicant_details_id=data["applicant_details"]["id"],
-                degree=data["education"][entry]["degree"],
-                stream=data["education"][entry]["stream"],
-                institute_name=data["education"][entry]["institute_name"],
-                institute_location=data["education"][entry]["institute_location"],
-                academic_year_start_date=data["education"][entry]["academic_year_start_date"],
-                academic_year_end_date=data["education"][entry]["academic_year_end_date"]
+                applicant_details_id=data["applicant_details"].get("id"),
+                degree=data["education"][entry].get("degree"),
+                stream=data["education"][entry].get("stream"),
+                institute_name=data["education"][entry].get("institute_name"),
+                institute_location=data["education"][entry].get("institute_location"),
+                academic_year_start_date=data["education"][entry].get("academic_year_start_date"),
+                academic_year_end_date=data["education"][entry].get("academic_year_end_date")
             )
             if education:
                 session.add(education)
     else:
         education = Education(
-            applicant_details_id=data["applicant_details"]["id"],
-            degree=data["education"][entry]["degree"],
-            stream=data["education"][entry]["stream"],
-            institute_name=data["education"][entry]["institute_name"],
-            institute_location=data["education"][entry]["institute_location"],
-            academic_year_start_date=data["education"][entry]["academic_year_start_date"],
-            academic_year_end_date=data["education"][entry]["academic_year_end_date"]
+            applicant_details_id=data["applicant_details"].get("id"),
+            degree=data["education"].get("degree"),
+            stream=data["education"].get("stream"),
+            institute_name=data["education"].get("institute_name"),
+            institute_location=data["education"].get("institute_location"),
+            academic_year_start_date=data["education"].get("academic_year_start_date"),
+            academic_year_end_date=data["education"].get("academic_year_end_date")
         )
         if education:
             session.add(education) 
@@ -156,48 +157,46 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
         places_worked = len(data["work_experience"])
         for entry in range(places_worked):
             work_experience = WorkExperience(
-                applicant_details_id=data["applicant_details"]["id"],
-                organization=data["work_experience"][entry]["organization"],
-                job_role=data["work_experience"][entry]["job_role"],
-                job_location=data["work_experience"][entry]["job_location"],
-                key_roles=data["work_experience"][entry]["key_roles"],
-                job_start_date=data["work_experience"][entry]["job_start_date"],
-                job_end_date=data["work_experience"][entry]["job_end_date"]
+                applicant_details_id=data["applicant_details"].get("id"),
+                organization=data["work_experience"][entry].get("organization"),
+                job_role=data["work_experience"][entry].get("job_role"),
+                job_location=data["work_experience"][entry].get("job_location"),
+                key_roles=data["work_experience"][entry].get("key_roles"),
+                job_start_date=data["work_experience"][entry].get("job_start_date"),
+                job_end_date=data["work_experience"][entry].get("job_end_date")
             )
             if work_experience:
-                print(work_experience)
                 session.add(work_experience)
     else:
         work_experience = WorkExperience(
-                applicant_details_id=data["applicant_details"]["id"],
-                organization=data["work_experience"]["organization"],
-                job_role=data["work_experience"]["job_role"],
-                job_location=data["work_experience"]["job_location"],
-                key_roles=data["work_experience"]["key_roles"],
-                job_start_date=data["work_experience"]["job_start_date"],
-                job_end_date=data["work_experience"]["job_end_date"]
+                applicant_details_id=data["applicant_details"].get("id"),
+                organization=data["work_experience"].get("organization"),
+                job_role=data["work_experience"].get("job_role"),
+                job_location=data["work_experience"].get("job_location"),
+                key_roles=data["work_experience"].get("key_roles"),
+                job_start_date=data["work_experience"].get("job_start_date"),
+                job_end_date=data["work_experience"].get("job_end_date")
             )
         if work_experience:
-            print(work_experience)
             session.add(work_experience)
     
     if type(data["social_media"]) is list:
         existing_accounts = len(data["social_media"])
         for entry in range(existing_accounts):
             social_media = SocialMedia(
-                applicant_details_id=data["applicant_details"]["id"],
-                media_name=data["social_media"][entry]["media_name"],
-                user_name=data["social_media"][entry]["user_name"],
-                url=data["social_media"][entry]["url"]
+                applicant_details_id=data["applicant_details"].get("id"),
+                media_name=data["social_media"][entry].get("media_name"),
+                user_name=data["social_media"][entry].get("user_name"),
+                url=data["social_media"][entry].get("url")
             )
             if social_media:
                 session.add(social_media)
     else:
         social_media = SocialMedia(
-                applicant_details_id=data["applicant_details"]["id"],
-                media_name=data["social_media"]["media_name"],
-                user_name=data["social_media"]["user_name"],
-                url=data["social_media"]["url"]
+                applicant_details_id=data["applicant_details"].get("id"),
+                media_name=data["social_media"].get("media_name"),
+                user_name=data["social_media"].get("user_name"),
+                url=data["social_media"].get("url")
             )
         if social_media:
                 session.add(social_media)
@@ -206,17 +205,17 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
         number_of_skills = len(data["skills"])
         for entry in range(number_of_skills):
             skills = Skills(
-                applicant_details_id=data["applicant_details"]["id"],
-                skill_name=data["skills"][entry]["skill_name"],
-                skill_level=data["skills"][entry]["skill_level"]
+                applicant_details_id=data["applicant_details"].get("id"),
+                skill_name=data["skills"][entry].get("skill_name"),
+                skill_level=data["skills"][entry].get("skill_level")
             )
             if skills:
                 session.add(skills)
     else:
         skills = Skills(
-            applicant_details_id=data["applicant_details"]["id"],
-            skill_name=data["skills"]["skill_name"],
-            skill_level=data["skills"]["skill_level"]
+            applicant_details_id=data["applicant_details"].get("id"),
+            skill_name=data["skills"].get("skill_name"),
+            skill_level=data["skills"].get("skill_level")
         )
         if skills:
             session.add(skills)
@@ -225,19 +224,19 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
         number_of_projects = len(data["projects"])
         for entry in range(number_of_projects):
             projects = Projects(
-                applicant_details_id=data["applicant_details"]["id"],
-                project_title=data["projects"][entry]["project_title"],
-                tools_used=data["projects"][entry]["tools_used"],
-                description=data["projects"][entry]["description"],     
+                applicant_details_id=data["applicant_details"].get("id"),
+                project_title=data["projects"][entry].get("project_title"),
+                tools_used=data["projects"][entry].get("tools_used"),
+                description=data["projects"][entry].get("description"),     
             )
             if projects:
                 session.add(projects)
     else:
         projects = Projects(
-                applicant_details_id=data["applicant_details"]["id"],
-                project_title=data["projects"]["project_title"],
-                tools_used=data["projects"]["tools_used"],
-                description=data["projects"]["description"],     
+                applicant_details_id=data["applicant_details"].get("id"),
+                project_title=data["projects"].get("project_title"),
+                tools_used=data["projects"].get("tools_used"),
+                description=data["projects"].get("description"),     
             )
         if projects:
             session.add(projects)
@@ -262,7 +261,7 @@ async def edit_data(applicant_id: int, data: dict[str, Any]) -> str:
     session.close()
     return "Updated"
 
-@delete("/delete-data/{applicant_id: int}")
+@delete("/delete-resume/{applicant_id: int}")
 async def delete_data(applicant_id: int) -> None:
     query = session.query(ApplicantDetails).filter_by(id=applicant_id).first()
     if query:
