8c4010cf09970b1b0da3a1ec937950f0fdb1fa86
sreeharipalokin <sreeharip@alokin.in>
2023-11-13 16:05:58 +0530

    else conditions removed and corrections made to get,post apis

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime
 from typing import Any
 from database import session
 from litestar import Litestar, get, post, put, delete, Request
@@ -13,23 +12,14 @@ def cleaned_record(record):
         record.pop(key)
     return record
 
-def list_of_dict(records):
-    """This method will return a list of dictionary if
-    there are multiple records present in a table which 
-    is related to a single id. 
+def final_data(records):
+    """This method is used to return data as a list of dictionaries 
+    for those fields where mutiple entries are possible in the input 
+    page.
+    Whether it have a single data or more than one data, the final
+    result will always be a list of dictionaries.
     """
-    final_list = []
-    for item in records:
-        record = item.__dict__
-        record = cleaned_record(record)
-        final_list.append(record)
-    return final_list
-
-def final_data_with_date(records):
-    """This method is used to process the records with date fields;
-    only to make sure that the date values from DB will be returned 
-    as string for effective json processing.
-    """
+    record_lis = []
     if records.count() == 1:
         record = records.first().__dict__
         record = cleaned_record(record)
@@ -37,27 +27,16 @@ def final_data_with_date(records):
         if record:
             for key,value in record.items():
                 new_record[key] = str(value)
-        return new_record
+            record_lis.append(new_record)
+        return record_lis
+    
     elif records.count() > 1:
-        record_lis = []
         for record in records.all():
             record = cleaned_record(record.__dict__)
             for key,value in record.items():
                 record[key] = str(value)
             record_lis.append(record)
         return record_lis
-  
-def final_data(record):
-    """This method will return the data as a dictionary if
-    only a single record exists in the table belonging to a 
-    particular id or else it will return a list of dictionary 
-    as data.
-    """
-    if record.count() == 1:
-        record = cleaned_record(record.first().__dict__)
-    else:
-        record = list_of_dict(record.all())
-    return record
 
 @get("/resumes/", name="get_all_resumes")
 async def show_all_data() -> json:
@@ -80,32 +59,43 @@ async def show_all_data() -> json:
 async def show_data_by_id(field_id: int) -> json:
     """This function will fetch records according to their id."""
 
-    all_data = {}  # dict to store all records
+    all_data = {}  # dictionary to store all records
 
-    # for storing applicant details record
+    """The data in the corresponding tables are collected here to 
+    their respective variables. After that the data is returned 
+    in the form of a key-value pair using the 'all_data' dictionary.
+    """
+
+    # Collecting ApplicantDetails data
     applicant_record = session.query(ApplicantDetails).filter_by(id=field_id).first().__dict__
     applicant_record.pop("_sa_instance_state", None)
-    # saving it in the dict
     all_data["applicant_details"] = applicant_record
 
+    # Collecting AddressDetails data    
     address_record = session.query(AddressDetails).filter_by(applicant_details_id=field_id).first().__dict__
     all_data["address_details"] = cleaned_record(address_record)
 
+    # Collecting Skills data    
     skills_record = session.query(Skills).filter_by(applicant_details_id=field_id)
     all_data["skills"] = final_data(skills_record)
 
+    # Collecting Projects data    
     projects_record = session.query(Projects).filter_by(applicant_details_id=field_id)
     all_data["projects"] = final_data(projects_record)
 
+    # Collecting SocialMedia data        
     media_record = session.query(SocialMedia).filter_by(applicant_details_id=field_id)
     all_data["social_media"] = final_data(media_record)
     
+    # Collecting Education data       
     education_record = session.query(Education).filter_by(applicant_details_id=field_id)
-    all_data["education"] = final_data_with_date(education_record)
+    all_data["education"] = final_data(education_record)
     
+    # Collecting WorkExperience data      
     work_record = session.query(WorkExperience).filter_by(applicant_details_id=field_id)
-    all_data["work_experience"] = final_data_with_date(work_record)
-
+    all_data["work_experience"] = final_data(work_record)
+ 
+    # Returning the collected data in the form of JSON
     json_data = json.dumps(all_data)
     return json_data
 
@@ -120,10 +110,9 @@ async def show_data_by_field(field_val: str) -> json:
 
 @post("/new-resume")
 async def add_data(request: Request, data:  dict[str, Any]) -> str:
-    """This function will save the applicant's details 
-    to the Database.
-    'applicant_details' is a variable which saves data
-    to ApplicantDetails model.
+    """This function will save the incoming data to the DB, to their
+    corresponding tables. 'applicant_details' is an object of ApplicantDetails 
+    model which is used to save the data to ApplicantDetails model.
     """
     applicant_details  = ApplicantDetails(
         id=data["applicant_details"].get("id"), 
@@ -136,7 +125,7 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
     if applicant_details:
         session.add(applicant_details)
 
-    #  address_details variable saves data to AddressDetails model
+    #  address_details object which saves data to AddressDetails model
     address_details = AddressDetails(
         applicant_details_id=data["applicant_details"].get("id"),
         address_line=data["address_details"].get("address_line"),
@@ -148,7 +137,9 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
     if address_details:
         session.add(address_details)
     
-    if type(data["education"]) is list:
+    # Here the values are retrieved from a nested dictionary
+    # The input data resides in a list belonging to the parent dictionary.
+    if data["education"]:
         levels_of_education = len(data["education"])
         for entry in range(levels_of_education):
             education = Education(
@@ -162,20 +153,8 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
             )
             if education:
                 session.add(education)
-    else:
-        education = Education(
-            applicant_details_id=data["applicant_details"].get("id"),
-            degree=data["education"].get("degree"),
-            stream=data["education"].get("stream"),
-            institute_name=data["education"].get("institute_name"),
-            institute_location=data["education"].get("institute_location"),
-            academic_year_start_date=data["education"].get("academic_year_start_date"),
-            academic_year_end_date=data["education"].get("academic_year_end_date")
-        )
-        if education:
-            session.add(education) 
 
-    if type(data["work_experience"]) is list:
+    if data["work_experience"]:
         places_worked = len(data["work_experience"])
         for entry in range(places_worked):
             work_experience = WorkExperience(
@@ -189,20 +168,8 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
             )
             if work_experience:
                 session.add(work_experience)
-    else:
-        work_experience = WorkExperience(
-                applicant_details_id=data["applicant_details"].get("id"),
-                organization=data["work_experience"].get("organization"),
-                job_role=data["work_experience"].get("job_role"),
-                job_location=data["work_experience"].get("job_location"),
-                key_roles=data["work_experience"].get("key_roles"),
-                job_start_date=data["work_experience"].get("job_start_date"),
-                job_end_date=data["work_experience"].get("job_end_date")
-            )
-        if work_experience:
-            session.add(work_experience)
     
-    if type(data["social_media"]) is list:
+    if data["social_media"]:
         existing_accounts = len(data["social_media"])
         for entry in range(existing_accounts):
             social_media = SocialMedia(
@@ -213,17 +180,8 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
             )
             if social_media:
                 session.add(social_media)
-    else:
-        social_media = SocialMedia(
-                applicant_details_id=data["applicant_details"].get("id"),
-                media_name=data["social_media"].get("media_name"),
-                user_name=data["social_media"].get("user_name"),
-                url=data["social_media"].get("url")
-            )
-        if social_media:
-                session.add(social_media)
     
-    if type(data["skills"]) is list:
+    if data["skills"]:
         number_of_skills = len(data["skills"])
         for entry in range(number_of_skills):
             skills = Skills(
@@ -233,16 +191,8 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
             )
             if skills:
                 session.add(skills)
-    else:
-        skills = Skills(
-            applicant_details_id=data["applicant_details"].get("id"),
-            skill_name=data["skills"].get("skill_name"),
-            skill_level=data["skills"].get("skill_level")
-        )
-        if skills:
-            session.add(skills)
 
-    if type(data["projects"]) is list:
+    if data["projects"]:
         number_of_projects = len(data["projects"])
         for entry in range(number_of_projects):
             projects = Projects(
@@ -253,16 +203,8 @@ async def add_data(request: Request, data:  dict[str, Any]) -> str:
             )
             if projects:
                 session.add(projects)
-    else:
-        projects = Projects(
-                applicant_details_id=data["applicant_details"].get("id"),
-                project_title=data["projects"].get("project_title"),
-                tools_used=data["projects"].get("tools_used"),
-                description=data["projects"].get("description"),     
-            )
-        if projects:
-            session.add(projects)
-
+                
+    # The received data is commited to the database
     session.commit()
     session.close()
     return "Record added successfully."
